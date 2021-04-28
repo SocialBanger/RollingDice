d4ac025279849b715efb02e7c947f069fef331dd
denhock <denhock@mail.ru>
2021-04-28 13:40:14 +0300

    Refactoring RandomService

--- rolledicerbot/src/main/java/io/github/socialbanger/rolldicebot/service/RandomService.java
+++ rolledicerbot/src/main/java/io/github/socialbanger/rolldicebot/service/RandomService.java
@@ -3,11 +3,12 @@ package io.github.socialbanger.rolldicebot.service;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class RandomService {
 
     public int getRoll(int diceType) {
-        return new Random().nextInt(diceType) + 1;
+        return ThreadLocalRandom.current().nextInt(diceType) + 1;
     }
 }
